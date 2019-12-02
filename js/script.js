fa7f62a8b4cafa6f0827ca9c58062805413c6917
Galata Tona <tonagalata+2@gmail.com>
2019-12-02 01:56:51 -0500

    adds save, delete and login features

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@
 
 let $foodSearch = '';
 
+$('span.userName')[0].innerText = JSON.parse(localStorage.getItem('userName'));
+$('span.userEmail')[0].innerText = JSON.parse(localStorage.getItem('email'));
 // function clearList(){
 
 // }
@@ -27,24 +29,60 @@ const $fieldSet = {
   'totalServing': 'nf_servings_per_container'
 }
 
-let $calIntakeNum = null;
-let $bCals = null;
-let $lCals = null;
-let $dCals = null;
-let $sCals = null;
+let $calIntakeNum = null, $bCals = null, $lCals = null, $dCals = null, $sCals = null, $fats = null, $carbs = null, $protein = null, $sugar = null, $totalCalories = null, $totalDailyCalories = parseInt($('#goal-cal').val());
+// parseInt(prompt('What is your daily calorie goal...'));
 
-let $totalCalories = null
-let $totalDailyCalories = parseInt(prompt('What is your daily calorie goal...'));
+function deleteEntry(evt) {
+  let $removeEntry = event.target.offsetParent;
+  
+  $removeEntry.remove();
+
+}
+
+let $diaryEntry = [];
+
+$('#save').on('click', (evt) => {
+  evt.preventDefault();
+  if($diaryEntry.length === 0) return;
+
+  for(let i=0; i < $diaryEntry.length; i++){
+    $diaryEntry[i]
+    localStorage.setItem(`Food Entry: ${i}`, $diaryEntry[i])
+    
+  }
+
+})
+
+$('#clear').on('click', (evt) => {
+  evt.preventDefault();
+  for(let i=0; i < localStorage.length; i++){
+  if(localStorage.getItem(`Food Entry: ${i}`) === null) return;
+    localStorage.clear(`Food Entry: ${i}`)
+  }
+
+})
 
 function clicked() {
   // event.preventDefault();
 
+  console.log(event.target.offsetParent.getElementsByClassName('submit-div')[0].innerHTML)
+
+
+
+  $diaryEntry.push(JSON.stringify(event.target.offsetParent.getElementsByClassName('submit-div')[0].innerHTML))
+  
+  console.log(JSON.stringify(event.target.offsetParent.getElementsByClassName('submit-div')[0].innerHTML))
+
+  // console.log($diaryEntry)
+
+
   let $addFoodText = event.target.offsetParent.getElementsByClassName('submit-div')[0].innerHTML;
-  console.log(event.target.offsetParent.getElementsByClassName('submit-div')[0]);
-  console.log(event.target.offsetParent.getElementsByClassName('sugar')[0].innerText);
-  console.log(event.target.offsetParent.getElementsByClassName('protein')[0].innerText);
-  console.log(event.target.offsetParent.getElementsByClassName('carbs')[0].innerText);
-  console.log(event.target.offsetParent.getElementsByClassName('totalFat')[0].innerText);
+  // console.log(event.target.offsetParent.getElementsByClassName('submit-div')[0]);
+  console.log('This one... ' + event.target.offsetParent.getElementsByClassName('sugar')[0].innerText);
+  console.log(event.target.offsetParent.getElementsByClassName('protein')[0].innerHTML);
+  let $itemProtein = event.target.offsetParent.getElementsByClassName('protein')[0].innerHTML;
+  let $itemCarbs = event.target.offsetParent.getElementsByClassName('carbs')[0].innerHTML;
+  let $itemFats = event.target.offsetParent.getElementsByClassName('totalFat')[0].innerHTML;
   let $itemCals = event.target.offsetParent.getElementsByClassName('calories')[0].innerHTML;
   // console.log(event.target.offsetParent.getElementsByClassName('submit-div')[0].innerHTML)
 
@@ -52,38 +90,42 @@ function clicked() {
   $addFood = $addFoodText
   // $('li.search-result').closest('li')
   
-  $('ul.daily-meals').append(`<li class="waves-effect col s12"><a class="btn waves-effect red">Delete</a><div class="list-text  col s8">${$addFood}</div></li>`)
+  $('ul.daily-meals').append(`<li class="food-entries waves-effect collection-item col s12"><a class="btn waves-effect red" onclick="deleteEntry()">Delete</a><div class="list-text  col s8">${$addFood}</div></li>`)
 
   $calIntakeNum++
   
   let $bSetDailyCal, $lSetDailyCal, $dSetDailyCal, $sSetDailyCal;
 
   if($calIntakeNum <= 3){
+    
     $bCals += parseInt($itemCals)
+    $carbs += parseInt($itemCarbs)
+    $fats += parseInt($itemFats)
+    $protein += parseInt($itemProtein)
     // console.log(myChart2.config.data.datasets[0].data[0]);
     $bSetDailyCal = parseInt((($bCals / $totalDailyCalories)*100));
-    
+    sessionStorage.setItem('b_calories', $bCals);
     $('#progress-bar').css('width', `${$bSetDailyCal}%`);
     $('#total-percent').css({'font-size': 'small'});
     $('#total-percent').text(`${$bCals}(kcal) ${$bSetDailyCal}%`);
   } else if($calIntakeNum > 3 && $calIntakeNum <= 6){
     $lCals += parseFloat($itemCals)
     $lSetDailyCal = parseInt(((($bCals + $lCals) / $totalDailyCalories)*100));
-    
+    sessionStorage.setItem('l_calories', $lCals);
     $('#progress-bar').css('width', `${$bSetDailyCal + $lSetDailyCal}%`);
     $('#total-percent').css({'font-size': 'small'});
     $('#total-percent').text(`${$bCals + $lCals}(kcal) ${$lSetDailyCal}%`);
   } else if($calIntakeNum > 6 && $calIntakeNum <= 9){
+    $dCals += parseFloat($itemCals)
     $dSetDailyCal = parseInt(((($bCals + $lCals + $dCals) / $totalDailyCalories)*100));
-    
+    sessionStorage.setItem('d_calories', $dCals);
     $('#progress-bar').css('width', `${$bSetDailyCal + $lSetDailyCal + $dSetDailyCal}%`);
     $('#total-percent').css({'font-size': 'small'});
     $('#total-percent').text(`${$bCals + $lCals + $dCals}(kcal) ${$dSetDailyCal}%`);
-    $dCals += parseFloat($itemCals)
   } else if($calIntakeNum <= 10){
     $sCals += parseFloat($itemCals)
     $sSetDailyCal = parseInt(((($bCals + $lCals + $dCals + $sCals) / $totalDailyCalories)*100));
-    
+    sessionStorage.setItem('s_calories', $sCals);
     $('#progress-bar').css('width', `${$bSetDailyCal + $lSetDailyCal + $dSetDailyCal + $sSetDailyCal}%`);
     $('#total-percent').css({'font-size': 'small'});
     $('#total-percent').text(`${$bCals + $lCals + $dCals + $sCals}(kcal) ${$sSetDailyCal}%`);
@@ -140,134 +182,6 @@ let myChart2 = new Chart(ctx2, {
     }
 });
 
-}
-
-$('#auto-search').on('click', (evt) => {
-  evt.preventDefault();
-
-  if(!$('#search-list div')){
-    return;
-  }else{
-  for(let i = 0; i < 20; i++){
-    $('#search-list div').remove(i);
-  }
-  }
-  $foodSearch = $('#autocomplete-input').val();
-  
-  const $query = JSON.stringify($foodSearch)
-
-  console.log(evt)
-  console.log(JSON.stringify($foodSearch))
-
-const promise = $.ajax({
-    url:`https://api.nutritionix.com/v1_1/search/${$query}?results=${$fieldSet.results}&fields=${$fieldSet.brandName},${$fieldSet.itemName},${$fieldSet.fats},${$fieldSet.carbs},${$fieldSet.protein},${$fieldSet.sugar},${$fieldSet.calories},${$fieldSet.totalServing}&appId=${$creds.appId}&appKey=${$creds.appKey}`,
-    success: (data) => {
-        console.log(data);
-    },
-    error: (error) => {
-          console.log('bad request: ', error);
-      }
-    });
-
-  promise.then( (data) => {
-    console.log(data)
-    console.log(data.hits)
-    console.log(data.hits[0].fields.item_name)
-
-    for(let i = 0; i < 20; i++){
-      $('ul#search-list').append(`<div class="search-add"><li class="waves-effect col s12 search-result"><button class="sub-button btn right waves-effect" onclick="clicked()">Add</button><div class="submit-div list-text col s8"><dl> 
-      <dt>Brand:</dt><dd class="list-item">${data.hits[i].fields.brand_name}</dd> 
-      <dt>Name:</dt><dt></dt><dd>${data.hits[i].fields.item_name}</dd>
-      <dt>Nutrients</dt>
-      <dd>Calories: <span class="calories">${data.hits[i].fields.nf_calories}</span> kcal</dd> 
-      <dd>Protein: <span class="protein">${data.hits[i].fields.nf_protein}</span> (g)</dd> 
-      <dd>Sugar: <span class="sugar">${data.hits[i].fields.nf_sugars}</span> (g)</dd> 
-      <dd>Carbs: <span  class="carbs">${data.hits[i].fields.nf_total_carbohydrate}</span> (g)</dd>
-      <dd>Total Fat: <span class="totalFat">${data.hits[i].fields.nf_total_fat}</span> (g)</dd>
-      <dd>Serv. Size: <span class="servSize">${data.hits[i].fields.nf_serving_size_qty}</span></dd>
-      <dd>Total Servings: <span class="TotalServ">${data.hits[i].fields.nf_servings_per_container}</span></dd></dl>
-      </div></li></div>`)
-    }
-
-  });
-});
-
-{/* 
-  <button class="btn col s2 right waves-effect">Add</button><div><li class="waves-effect col s10 search-result">
-      <div class="list-text col s8"> 
-      <dt>Brand:</dt><dd class="list-item">${data.hits[i].fields.brand_name}</dd></div>
-      </li></div>
-  <div class="list-text col s8"> 
-<dt>Brand:</dt><dd class="list-item">${data.hits[i].fields.brand_name}</dd> 
-<dt>Name:<dt><dd>${data.hits[i].fields.item_name}</dd>
-<dt>Nutrients</dt>
-<dd class="list-item">Calories: ${data.hits[i].fields.nf_calories} kcal</dd> 
-<dd class="list-item">Protein: ${data.hits[i].fields.nf_protein} (g)</dd> 
-<dd class="list-item">Sugar: ${data.hits[i].fields.nf_sugars} (g)</dd> 
-<dd class="list-item">Carbs: ${data.hits[i].fields.nf_total_carbohydrate} (g)</dd>
-<dd class="list-item">Total Fat: ${data.hits[i].fields.nf_total_fat} (g)</dd>
-<dd class="list-item">Serv. Size: ${data.hits[i].fields.nf_serving_size_qty}</dd>
-<dd class="list-item">Total Servings: ${data.hits[i].fields.nf_servings_per_container}</dd>
-</div> */}
-
-
-// for(let i=1; i <= $countItems; i++){
-// $('ul.daily-meals').append(`<li class="waves-effect col s12"><a href="#" class="btn waves-effect">List Item 2</a><div class="list-text  col s8">Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text!</div></li>`)
-// }
-
-
-  $('#progress-bar').css('width', '20%');
-  $('#progress-bar').removeClass('green').addClass('red')
-  $('#total-percent').text('20%');
-
-  $('#progress-bar2').css('width', '40%');
-  $('#progress-bar2').removeClass('green').addClass('yellow')
-  $('#goal-percent').text('40%');
-
-
-
-document.addEventListener("touchstart", function(){}, true)
-
-let ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, {
-    title:{
-      text: "Macronutrients"   
-    },
-    type: 'doughnut',
-    data: {
-        labels: ['Fats', 'Carbs', 'Protein'],
-        datasets: [{
-            label: '% of macronutrients',
-            data: ['20', '40', '40'],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.5)',
-                'rgba(54, 162, 235, 0.5)',
-                'rgba(255, 206, 86, 0.5)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-      legend: {
-            position: 'left',
-            labels: {
-              fontColor: "black",
-              boxWidth: 20,
-              padding: 20
-          }
-          }
-      }
-});
-
-
-
-
-
 let ctx3 = document.getElementById('myChart3').getContext('2d');
 let myChart3 = new Chart(ctx3, {
     type: 'horizontalBar',
@@ -275,7 +189,7 @@ let myChart3 = new Chart(ctx3, {
         labels: ['Breakfast', 'Lunch', 'Dinner', 'Snacks'],
         datasets: [{
             label: 'Calories by Meal',
-            data: [25, 32, 37, 10],
+            data: [$bCals, $lCals, $dCals, $sCals],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.5)',
                 'rgba(54, 162, 235, 0.5)',
@@ -319,6 +233,45 @@ let myChart3 = new Chart(ctx3, {
     }
 });
 
+// console.log(document.querySelector('.macros-chart').getContext('2d'))
+// console.log(document.querySelector('.meals-chart').getContext('2d'))
+
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    title:{
+      text: "Macronutrients"   
+    },
+    type: 'doughnut',
+    data: {
+        labels: ['Fats', 'Carbs', 'Protein'],
+        datasets: [{
+            label: '% of macronutrients',
+            data: [$fats, $carbs, $protein],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      legend: {
+            position: 'left',
+            labels: {
+              fontColor: "black",
+              boxWidth: 20,
+              padding: 20
+          }
+          }
+      }
+});
+
 let ctx4 = document.getElementById('myChart4').getContext('2d');
 let myChart4 = new Chart(ctx4, {
     title:{
@@ -329,7 +282,7 @@ let myChart4 = new Chart(ctx4, {
         labels: ['Fats', 'Carbs', 'Protein'],
         datasets: [{
             label: '% of macronutrients',
-            data: ['20', '40', '40'],
+            data: [$fats, $carbs, $protein],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.5)',
                 'rgba(54, 162, 235, 0.5)',
@@ -356,7 +309,84 @@ let myChart4 = new Chart(ctx4, {
 });
 
 
-let $countItems = 50;
+
+}
+
+$('#auto-search').on('click', (evt) => {
+  evt.preventDefault();
+
+  if(!$('#search-list div')){
+    return;
+  }else{
+  for(let i = 0; i < 20; i++){
+    $('#search-list div').remove(i);
+  }
+  }
+  $foodSearch = $('#autocomplete-input').val();
+  
+  const $query = JSON.stringify($foodSearch)
+
+  // console.log(evt)
+  // console.log(JSON.stringify($foodSearch))
+
+const promise = $.ajax({
+    url:`https://api.nutritionix.com/v1_1/search/${$query}?results=${$fieldSet.results}&fields=${$fieldSet.brandName},${$fieldSet.itemName},${$fieldSet.fats},${$fieldSet.carbs},${$fieldSet.protein},${$fieldSet.sugar},${$fieldSet.calories},${$fieldSet.totalServing}&appId=${$creds.appId}&appKey=${$creds.appKey}`,
+    success: (data) => {
+        console.log(data);
+    },
+    error: (error) => {
+          console.log('bad request: ', error);
+      }
+    });
+
+  promise.then( (data) => {
+    // console.log(data)
+    // console.log(data.hits)
+    // console.log(data.hits[0].fields.item_name)
+
+    
+
+    for(let i = 0; i < 20; i++){
+      $('ul#search-list').append(`<div class="search-add"><li class="waves-effect col s12 search-result"><button class="sub-button btn right waves-effect" onclick="clicked()">Add</button><div class="submit-div list-text col s8"><dl> 
+      <dt>Brand:</dt><dd class="list-item">${data.hits[i].fields.brand_name}</dd> 
+      <dt>Name:</dt><dt></dt><dd>${data.hits[i].fields.item_name}</dd>
+      <dt>Nutrients</dt>
+      <dd>Calories: <span class="calories">${data.hits[i].fields.nf_calories}</span> kcal</dd> 
+      <dd>Protein: <span class="protein">${data.hits[i].fields.nf_protein}</span> (g)</dd> 
+      <dd>Sugar: <span class="sugar">${data.hits[i].fields.nf_sugars}</span> (g)</dd> 
+      <dd>Carbs: <span  class="carbs">${data.hits[i].fields.nf_total_carbohydrate}</span> (g)</dd>
+      <dd>Total Fat: <span class="totalFat">${data.hits[i].fields.nf_total_fat}</span> (g)</dd>
+      <dd>Serv. Size: <span class="servSize">${data.hits[i].fields.nf_serving_size_qty}</span></dd>
+      <dd>Total Servings: <span class="TotalServ">${data.hits[i].fields.nf_servings_per_container}</span></dd></dl>
+      </div></li></div>`)
+    }
+
+  });
+});
+
+
+// for(let i=1; i <= $countItems; i++){
+// $('ul.daily-meals').append(`<li class="waves-effect col s12"><a href="#" class="btn waves-effect">List Item 2</a><div class="list-text  col s8">Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text!</div></li>`)
+// }
+
+
+  $('#progress-bar').css('width', '0%');
+  $('#progress-bar').removeClass('green').addClass('red')
+  $('#total-percent').text('0%');
+
+  $('#progress-bar2').css('width', '0%');
+  $('#progress-bar2').removeClass('green').addClass('yellow')
+  $('#goal-percent').text('0%');
+
+
+
+document.addEventListener("touchstart", function(){}, true)
+
+$('#nav-mobile3').on('click', () => {
+  $('.sidenav').sidenav();
+});
+
+// let $countItems = 50;
 
 
 // for(let i=1; i <= $countItems; i++){
@@ -367,10 +397,89 @@ let $countItems = 50;
 //   $('#search-list').append(`<li class="waves-effect col s12"><a href="#" class="btn waves-effect">Food Item ${i}</a><div class="list-text  col s8">Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text! Hello this is a Galata food item ${i} text! </div></li>`)
 //   }
 
-  $('#nav-mobile3').on('click', () => {
-    $('.sidenav').sidenav();
-  });
+$(document).ready( () => {
+  userActivity();
+});
+
+function closeModel () {
+
+  let $parsed = [];
+
+  for(let i=0; i < localStorage.length; i++){
+  if(localStorage.getItem(`Food Entry: ${i}`) === null) 
+  {
+          document.querySelector('.bg-modal').style.display = "none";
+  return document.querySelector('body').style.overflow = "visible";
+  };
+
+  $parsed.push(localStorage.getItem(`Food Entry: ${i}`))
+  
+  $('ul.daily-meals').append(`<li class="food-entries waves-effect collection-item col s12"><a class="btn waves-effect red" onclick="deleteEntry()">Delete</a><div class="list-text  col s8">${JSON.parse($parsed[i])}</div></li>`)
+}
+
+}
+
+document.querySelector('#sign-up').addEventListener("click", (evt) => {
+  if(($('#userName').val() !== "") && ($('#userEmail').val() !== "") && ($('#userPassword').val() !== "")){
+
+    let $userName = JSON.stringify($('#userName').val())
+    localStorage.setItem('userName', $userName)
+    sessionStorage.setItem('userName', $userName)
+    $('span.userName')[0].innerText = $userName;
+    
+    let $userEmail = JSON.stringify($('#userEmail').val());
+    localStorage.setItem('email', $userEmail)
+    sessionStorage.setItem('email', $userEmail)
+    $('span.userEmail')[0].innerText = $userEmail;
+    
+    let $userPassword = JSON.stringify($('#userPassword').val());
+    localStorage.setItem('password', $userPassword)
+    sessionStorage.setItem('password', $userPassword)
+    
+    closeModel();
+  } else {
+    alert("Please complete the form to access the page...")
+  }
+});
+
+document.querySelector('#sign-in').addEventListener("click", (evt) => {
+  let $userName = JSON.stringify($('#userName').val());
+  let $userEmail = JSON.stringify($('#userEmail').val());
+  let $userPassword = JSON.stringify($('#userPassword').val());
+ 
+  userHistory($userEmail, $userPassword, $userName);
+});
 
 
+function userHistory(email, password, uName) {
+  if(((localStorage.getItem('email') === email) && (localStorage.getItem('password') === password) && (localStorage.getItem('userName') === uName))){
 
 
+    $('span.userName')[0].innerText = sessionStorage.getItem('userName');
+    $('span.userEmail')[0].innerText = sessionStorage.getItem('email');
+
+    let $totlCals = parseInt(localStorage.getItem('total_calories')) 
+    $totlCals = $totalDailyCalories;
+
+    sessionStorage.setItem('userName', uName)
+    sessionStorage.setItem('email', email)
+    sessionStorage.setItem('password', password)
+
+    closeModel();
+  }else{
+    alert('Sorry, no record found...\nPlease Sign-in with your credentials or\n Sign-up')
+    console.log('Sorry, no record found...\nPlease Sign-in with your credentials or\n Sign-up')
+  }
+}
+
+function userActivity(email, password, uName) {
+
+  if(((sessionStorage.getItem('email')) !== null) && ((sessionStorage.getItem('password')) !== null)  && ((sessionStorage.getItem('userName')) !== null)){
+    $totalDailyCalories = parseInt($('#goal-cal').val()); 
+    closeModel()
+  }else if(((sessionStorage.getItem('email') == email) && (sessionStorage.getItem('password') == password) && (sessionStorage.getItem('userName') == uName))){
+    document.querySelector('.bg-modal').style.display = "flex";
+  }else {
+    console.log('Sorry, no record found...\n\nPlease sign in')
+  }
+}
